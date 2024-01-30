890e6d34bb8c3f93839f79875fc5caf2a7130b84
shust <step.shustov@mail.ru>
2024-01-30 17:16:18 +0400

    commit 1

---
+++ addution.py
@@ -0,0 +1,220 @@
+import pygame
+import os
+
+LEFT = 30
+TOP = 200
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as f:
+        lev_map_ = [line.strip() for line in f]
+    max_width = max(map(len, lev_map_))
+    return list(map(lambda x: list(x.ljust(max_width, '.')), lev_map_))
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    return image
+
+
+def draw_buttons(mas, screen):
+    for i in range(len(mas)):
+        mas[i].draw(screen)
+
+
+class MySpritesGroup(pygame.sprite.Group):
+    def update_sprites(self, state):
+        for el in self:
+            el.update(state)
+
+    def resize(self, cs):
+        for el in self:
+            el.resize(cs)
+
+
+tile_images = {
+    'box': load_image('box.png'),
+    'empty': load_image('empty.png'),
+    'heart': load_image('heart.png')
+}
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, cs=0, *group):
+        super().__init__(*group)
+        self.rect = None
+        self.cs = cs
+
+    def get_event(self, event):
+        pass
+
+    def resize(self, cs):
+        self.cs = cs
+        self.rect.update(LEFT, TOP, cs, cs)
+
+
+class Tile(Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, spr_gr, cs):
+        super().__init__(cs, spr_gr)
+        self.image = tile_images[tile_type]
+        self.image = pygame.transform.scale(self.image, (cs, cs))
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+
+    def update(self, *args, **kwargs):
+        pass
+
+
+class Player(Sprite):
+    def __init__(self, x, y, spr_gr, cs, v=0):
+        super().__init__(cs, spr_gr)
+        self.images = [load_image(f'{i}.png') for i in range(1, 6)]
+        self.k = 0  # индекс изображение спрайта
+        self.cnt = 17  # счетчик количества обновлений для анимаций
+        self.v = v  # куда направлен герой? 0 - вдево ; 1 - впарво
+        self.state = 'main'
+        self.image = pygame.transform.scale(self.images[self.k], (cs, cs))
+        if v:
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect().move(x, y)
+        self.x = x
+        self.y = y
+
+    def move(self, x, y):
+        self.x, self.y = x, y
+        self.rect = self.image.get_rect().move(self.x, self.y)
+
+    def update(self, *args, **kwargs):
+        self.image = pygame.transform.scale(self.images[self.k], (self.cs, self.cs))
+        if self.v:
+            self.image = pygame.transform.flip(self.image, True, False)
+        if self.state == 'main':
+
+            self.cnt += 1
+            if self.cnt >= 18:
+                self.cnt = 0
+                if self.k != 0:
+                    self.k = 0
+                else:
+                    self.k = 1
+
+                self.rect = self.image.get_rect()
+                self.rect.x = self.x
+                self.rect.y = self.y
+            return
+        if self.state == "attack":
+            self.cnt += 1
+            if self.cnt >= 20:
+                self.state = "main"
+                return
+            self.k = 3
+
+
+class Prizrak(Sprite):
+    def __init__(self, x, y, spr_gr, cs, v=0, cnt_mx=20):
+        super().__init__(cs, spr_gr)
+        self.orig_im = load_image(f'prizrak.png')
+        self.v = v  # куда направлен герой? 0 - вдево ; 1 - впарво
+        self.cnt = 15  # счетчик количества обновлений для анимаций
+        self.xp = 2  # для анимации
+        self.yp = 1  # для анимации
+        self.k = 1  # для анимации
+        self.cmt_mx = cnt_mx
+        self.image = pygame.transform.scale(self.orig_im, (cs, cs))
+        if v:
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect().move(x, y)
+        self.x = x
+        self.y = y
+
+    def update(self, *args, **kwargs):
+        self.cnt += 1
+        if self.cnt == self.cmt_mx:
+            self.cnt = 0
+            self.k += 1
+            self.x = self.rect.x = self.x + self.xp
+            self.y = self.rect.y = self.y + self.yp
+            self.xp *= -1
+            self.yp *= -1
+
+
+class Button:
+    def __init__(self, name: str, x, y, w, h,
+                 color_rect='#ff0000', text="",
+                 color_txt='#00ff00', sh=0):
+        self.x = int(x)
+        self.y = int(y)
+        self.w = int(w)
+        self.h = int(h)
+        self.pos = self.x, self.y
+        self.name = name
+        self.color_rect = color_rect
+        self.color_txt = color_txt
+        self.size = self.w, self.h = w, h
+        self.text = text
+        self.sh = sh
+
+    def update(self, old_w, old_h, new_w, new_h):
+        self.pos = self.x, self.y = int(new_w * self.x / old_w), int(new_h * self.y / old_h)
+        self.size = self.w, self.h = int(new_w * self.w / old_w), int(new_h * self.h / old_h)
+        self.sh = int(self.sh * new_w * new_h / old_h / old_w)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color_rect, (self.x, self.y, self.w, self.h), 2)
+        if self.sh:
+            font = pygame.font.Font(None, self.sh)
+            text = font.render(self.text, True, self.color_txt)
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (self.x + abs(self.w - text_w) // 2, self.y + abs(self.h - text_h) // 2))
+
+    def check_click(self, x, y):
+        return (self.x <= x <= self.x + self.w) and (self.y <= y <= self.y + self.h)
+
+    def change_text(self, text, color_txt, sh):
+        self.text, self.color_txt, self.sh = text, color_txt, sh
+
+
+def is_go(lvl_mp: list, x, y):
+    if 0 <= x < len(lvl_mp) and 0 <= y < len(lvl_mp[x]):
+        if lvl_mp[x][y] == '.':
+            return True
+    return False
+
+
+def get_coord(x, y, cs):
+    return (y - TOP) // cs, (x - LEFT) // cs
+
+
+class Health:
+    def __init__(self, n=3, mx=5):
+        self.spr_gr = MySpritesGroup()
+        self.hp = n  # кол-во жизней
+        self.max_hp = mx
+        self.sprites = []
+        for i in range(n):
+            self.sprites.append(Tile('heart', i * 45 + 20, 10, self.spr_gr, 50))
+
+    def plus(self):
+        self.hp = min(self.max_hp, self.hp + 1)
+        self.sprites.append(Tile('heart', self.hp * 45 - 25, 10, self.spr_gr, 50))
+
+    def minus(self):
+        self.hp -= 1
+        if self.hp <= 0:
+            return False
+        self.sprites.pop().kill()
+        return True
+
+    def update(self, screen):
+        self.spr_gr.draw(screen)
+
+    def delete(self):
+        self.hp = 0
+        while len(self.sprites):
+            self.sprites.pop().kill()
+
+
+if __name__ == '__main__':
+    print(load_level("map1.map"))
---
+++ main.py
@@ -0,0 +1,254 @@
+import pygame
+import os
+from addution import *  # файл со спрайтами кнопками и прочем
+import random
+from queue import Queue
+
+# минимальные размеры окна
+MINWIDTH = 800
+MINHEIGHT = 450
+
+
+def moving_enemies(x, y):
+    global health
+    global state
+    levmp_cop = []
+    for i in range(len(level_map)):
+        levmp_cop.append(level_map[i].copy())
+    for i in range(len(level_map)):
+        for j in range(len(level_map[i])):
+            if levmp_cop[i][j] == 'p':
+                levmp_cop[i][j] = '.'
+                tx, ty = bfs((i, j), (x, y))
+                if tx != -1 and ty != -1:
+                    level_map[tx][ty] = 'p'
+                    level_map[i][j] = '.'
+                if tx == x and ty == y:
+                    level_map[tx][ty] = '@'
+                    if not health.minus():
+                        return "died"
+
+
+def bfs(start: tuple, finish: tuple):
+    dist = [[10000 for j in range(w_l)] for i in range(h_l)]
+    dist[finish[0]][finish[1]] = 0
+    q = Queue()
+    q.put(finish)
+    cnt = 0
+    while q.qsize():
+        cnt += 1
+        v = q.get()
+        if v[0] + 1 < h_l and dist[v[0] + 1][v[1]] == 10000 and level_map[v[0] + 1][v[1]] == '.':
+            q.put((v[0] + 1, v[1]))
+            dist[v[0] + 1][v[1]] = dist[v[0]][v[1]] + 1
+
+        if v[0] - 1 >= 0 and dist[v[0] - 1][v[1]] == 10000 and level_map[v[0] - 1][v[1]] == '.':
+            q.put((v[0] - 1, v[1]))
+            dist[v[0] - 1][v[1]] = dist[v[0]][v[1]] + 1
+
+        if v[1] + 1 < w_l and dist[v[0]][v[1] + 1] == 10000 and level_map[v[0]][v[1] + 1] == '.':
+            q.put((v[0], v[1] + 1))
+            dist[v[0]][v[1] + 1] = dist[v[0]][v[1]] + 1
+
+        if v[1] - 1 > 0 and dist[v[0]][v[1] - 1] == 10000 and level_map[v[0]][v[1] - 1] == '.':
+            q.put((v[0], v[1] - 1))
+            dist[v[0]][v[1] - 1] = dist[v[0]][v[1]] + 1
+
+        if dist[start[0]][start[1]] != 10000:
+            break
+    res = (-1, -1)
+    if start[0] - 1 >= 0:
+        res = (start[0] - 1, start[1])
+    elif start[1] - 1 >= 0:
+        res = (start[0], start[1] - 1)
+    elif start[0] + 1 < h_l:
+        res = (start[0] + 1, start[1])
+    else:
+        res = (start[0], start[1] + 1)
+
+    if start[0] - 1 >= 0 and dist[start[0] - 1][start[1]] < dist[res[0]][res[1]]:
+        res = (start[0] - 1, start[1])
+    if start[1] - 1 >= 0 and dist[start[0]][start[1] - 1] < dist[res[0]][res[1]]:
+        res = (start[0], start[1] - 1)
+    if start[0] + 1 < h_l and dist[start[0] + 1][start[1]] < dist[res[0]][res[1]]:
+        res = (start[0] + 1, start[1])
+    if start[1] + 1 < w_l and dist[start[0]][start[1] + 1] < dist[res[0]][res[1]]:
+        res = (start[0], start[1] + 1)
+
+    return res
+
+
+def gen_lev(v=0):
+    global all_sprites
+    global cell_size
+    global h_l, w_l
+    h_l, w_l = len(level_map), len(level_map[0])
+    cell_size = int(min((width - LEFT - 20) / w_l, (height - TOP - 20) / h_l))
+
+    all_sprites = MySpritesGroup()
+    new_player, x, y = -1, -1, -1
+    for k in range(len(level_map)):
+        for j in range(len(level_map[k])):
+            if level_map[k][j] == '#':
+                Tile('box', LEFT + j * cell_size, TOP + k * cell_size, all_sprites, cell_size)
+            elif level_map[k][j] == '@':
+                new_player = Player(LEFT + j * cell_size, TOP + k * cell_size, all_sprites, cell_size, v)
+                x, y = k, j
+            elif level_map[k][j] == 'p':
+                Prizrak(LEFT + j * cell_size, TOP + k * cell_size, all_sprites, cell_size, v=0,
+                        cnt_mx=random.randint(20, 30))
+    return new_player, x, y
+
+
+if __name__ == '__main__':
+    state = "start_window"  # переменная в которой хранится состояние (FSM на минималках)
+    pygame.init()
+    size = width, height = 1280, 720
+    screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    running = True
+    clock = pygame.time.Clock()
+    all_sprites = MySpritesGroup()
+    # pl = Player(20, 50, all_sprites, 75)
+    # Tile('box', 100, 400, all_sprites, 50)
+
+    cnt_an = 30  # счетчик задержки для анимаций
+
+    v_ = 0
+    level = -1
+    level_map = [['.']]
+    x_pos = y_pos = -1
+    size_lev = w_lev, h_lev = -1, -1
+    cell_size = 60
+    buttons = list()  # список всех кнопок
+    buttons.append(Button("play", width * 0.4, height * 0.4, 150, 60, text="Play", sh=50))
+    orig_fon = load_image('fon.jpg')
+    fon = pygame.transform.scale(orig_fon, (width, height))  # задний фон игры
+    while running:
+        cnt_an = min(30, cnt_an + 1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if cnt_an < 30:
+                continue
+            if state == "start_window":
+                if event.type == pygame.VIDEORESIZE:  # изменение размеров окна и размеров всех объектов
+                    old_size = size
+                    width, height = pygame.display.get_surface().get_size()
+                    width = max(width, MINWIDTH)
+                    height = max(height, MINHEIGHT)
+                    size = width, height
+                    for el in buttons:
+                        el.update(*old_size, *size)
+                    fon = pygame.transform.scale(orig_fon, (width, height))
+                    screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in range(len(buttons)):
+                        if buttons[i].check_click(*event.pos):
+                            if buttons[i].name == "play":
+                                state = "select_level"
+                                buttons = []
+                                all_sprites = MySpritesGroup()
+                                cnt = 0
+                                for row in range(2):
+                                    for col in range(10):
+                                        cnt += 1
+                                        buttons.append(Button(f'level:{cnt}',
+                                                              col * 100 + 40, (row + 1) * 100,
+                                                              90, 90,
+                                                              '#ffffff', f"{cnt}",
+                                                              "#ffffff", 30))
+
+                            break
+
+            if state == "select_level":
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in range(len(buttons)):
+                        if buttons[i].check_click(*event.pos):
+                            but = buttons[i].name.split(':')
+                            if but[0] == "level":
+                                level = int(but[1])
+                                try:
+                                    level_map = load_level(f"map{level}.map")
+                                    buttons = []
+
+                                    h_l, w_l = len(level_map), len(level_map[0])
+                                    player, x_pos, y_pos = gen_lev()
+                                    health = Health(3)
+                                    state = "level_go"
+                                except Exception as ex:
+                                    print("file not find or error:\n", ex)
+
+                            break
+
+            if state == "level_go":
+                if event.type == pygame.KEYDOWN:
+
+                    level_map[x_pos][y_pos] = '.'
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        if is_go(level_map, x_pos - 1, y_pos):
+                            x_pos -= 1
+                    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        if is_go(level_map, x_pos + 1, y_pos):
+                            x_pos += 1
+                    elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        v_ = 0
+                        if is_go(level_map, x_pos, y_pos - 1):
+                            y_pos -= 1
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        v_ = 1
+                        if is_go(level_map, x_pos, y_pos + 1):
+                            y_pos += 1
+                    level_map[x_pos][y_pos] = '@'
+                    if moving_enemies(x_pos, y_pos) == "died":
+                        health.delete()
+                        all_sprites = MySpritesGroup()
+                        state = "died"
+
+                    player, x_pos, y_pos = gen_lev(v_)
+
+                if event.type == pygame.VIDEORESIZE:  # изменение размеров окна и размеров всех объектов
+                    old_size = size
+                    width, height = pygame.display.get_surface().get_size()
+                    width = max(width, MINWIDTH)
+                    height = max(height, MINHEIGHT)
+                    size = width, height
+                    player, x_pos, y_pos = gen_lev(v_)
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    xc, yc = get_coord(*event.pos, cell_size)
+                    if 0 <= xc < h_l and 0 <= yc < w_l:
+                        if (abs(x_pos - xc) == 1 and abs(y_pos - yc) == 0) or (
+                                abs(x_pos - xc) == 0 and abs(y_pos - yc) == 1):
+                            if y_pos < yc:
+                                v_ = 1
+                            elif y_pos > yc:
+                                v_ = 0
+                            level_map[xc][yc] = '.'
+                            player, x_pos, y_pos = gen_lev(v_)
+                            player.cnt = 0
+                            player.state = "attack"
+                            cnt_an = 0
+                            if moving_enemies(x_pos, y_pos) == "died":
+                                health.delete()
+                                all_sprites = MySpritesGroup()
+                                state = "died"
+
+                            player, x_pos, y_pos = gen_lev(v_)
+
+        clock.tick(50)
+        screen.fill('#000000')
+        if state == "start_window":
+            screen.blit(fon, (0, 0))
+
+        draw_buttons(buttons, screen)  # отрисовка всех кнопок
+        all_sprites.draw(screen)
+        all_sprites.update(state)
+        if state == "level_go":
+            health.update(screen)
+            for k in range(len(level_map)):
+                for j in range(len(level_map[k])):
+                    if level_map[k][j] != '#':
+                        pygame.draw.rect(screen, 'white',
+                                         (LEFT + j * cell_size, TOP + k * cell_size, cell_size, cell_size), 1)
+
+        pygame.display.flip()
